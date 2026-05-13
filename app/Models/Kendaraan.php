18a2a74f46805fb7ae3898081a5c3ea174c1b344
lentamanalu <email@lentamanalu460@gmail.com>
2026-05-13 16:30:44 +0700

    konfirasi model

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     
+     * @var array<int, string>
+     */
+    protected $table = 'kendaraans';
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan'
+    ];
 }
